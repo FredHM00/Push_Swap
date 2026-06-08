a9d943ffd6afe50d922f5d1e5bb3be8ad7ff623d
Frederick Hegel Martins <fmartins@student.42sp.org.br>
2026-06-08 19:05:35 -0300

    feat(parsing): validate numeric input and int limits

---
+++ src/parsing/is_valid_number.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_valid_number.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmartins <fmartins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/08 15:30:47 by fmartins          #+#    #+#             */
+/*   Updated: 2026/06/08 19:02:46 by fmartins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	error(void)
+{
+	ft_putendl_fd("Error", 1);
+}
+
+static int	check_int(long num)
+{
+	if (num > INT_MAX || num < INT_MIN)
+		return (0);
+	return (1);
+}
+
+static int	check_str(const char *str)
+{
+	int		sign;
+	int		num;
+	long	count;
+
+	sign = 1;
+	num = 0;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	count = 0;
+	while ((*str >= '0') && (*str <= '9'))
+	{
+		count = (count * 10) + (*str - '0');
+		str++;
+		num += 1;
+	}
+	if ((num <= 0) || (*str))
+		return (0);
+	if (check_int(count * sign))
+		return (1);
+	return (0);
+}
+
+int	is_valid_number(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (!check_str(av[i]))
+		{
+			error();
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
